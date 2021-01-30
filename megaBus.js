e28e3723990f4e3b7f474e3317e3e5c330ec594b
ranjit-mandal <ranjit.musician01@gmail.com>
2021-01-30 23:08:05 +0600

    un finished

--- megaBus.js
+++ megaBus.js
@@ -19,10 +19,66 @@ if(isIncrease == false && firstClassAmount > 0){
 }
 
     firstClassRate.value = firstClassNewAmount;
-    const firstClassTotal = firstClassNewAmount * 150;
-    document.getElementById('subtotal').innerText = firstClassTotal;
-
+    // const firstClassTotal = firstClassNewAmount * 150;
+    // document.getElementById('subtotal').innerText = firstClassTotal;
 }
 
+//economy part start
+/*
+document.getElementById('economyIncrease').addEventListener('click', function(){
+const economyRate = document.getElementById('economyPrice');
+const economyAmount = parseInt(economyRate.value);
+const economyNewAmount = economyAmount + 1;
+economyRate.value = economyNewAmount;
+});
 
+document.getElementById('economyDecrease').addEventListener('click', function(){
+    const economyRate = document.getElementById('economyPrice');
+    const economyAmount = parseInt(economyRate.value);
+    const economyNewAmount = economyAmount - 1;
+    economyRate.value = economyNewAmount;
+});
+*/
+//economy part start
+document.getElementById('economyIncrease').addEventListener('click', function(){
+    busCounter(true)        
+    });
+    
+    document.getElementById('economyDecrease').addEventListener('click', function(){
+        busCounter(false)       
+    });
+    
+    function busCounter(isIncrease){
+        const economyRate = document.getElementById('economyPrice');
+        const economyAmount = parseInt(economyRate.value);
+        //const economyNewAmount = economyAmount + 1;
+        let economyNewAmount = economyAmount;
+        if(isIncrease == true){
+            economyNewAmount = economyAmount + 1;
+        }
+        if(isIncrease == false && economyAmount>0 )
+        economyNewAmount = economyAmount - 1;
+        economyRate.value = economyNewAmount;
+    }
+        
+    function calculateTotal(){
+        const firstClassInput = documentById('firstClassPrice');
+        const firstClassCount = parseInt(firstClassInput.value);
+        const firstClassCost = firstClassCount;
 
+        const economyInput = documentById('economyPrice');
+        const economyCount = parseInt(economyInput.value);
+        const economyCost = economyCount;
+        
+
+        const subTotalPrice = firstClassCost*150 + economyCost*100;
+        document.getElementById('sub-total').innerText = subTotalPrice;
+
+        const vatInput = document.getElementById('total-vat');
+        const vatValue = subTotalPrice*0.1;
+        const totalVat = parseInt(vatValue);
+        document.getElementById('total-vat').innerText = totalVat;
+
+        const grandTotal = subTotalPrice + totalVat;
+        document.getElementById('total-price').innerText = grandTotal;  
+    }
